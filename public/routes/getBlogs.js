3c1bfa9910dd9fdf1d7193615be06cae7c515834
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-08 17:01:37 +0800

    added a button to navigate through the pages

--- public/routes/getBlogs.js
+++ public/routes/getBlogs.js
@@ -18,14 +18,14 @@ router.get('/', (req, res) => {
 router.get('/blogs', (req, res) => {
     const { page } = req.query
     let limit = 10
-    let skip = limit * (page - 1)
+    let skip = limit * (parseInt(page) - 1)
 
     Post.find({}, (err, foundBlogs) => {
         if (err) {
             req.flash('error', 'Something went wrong upon getting all the post from the databases');
-            res.redirect("/blogs");
+            res.redirect("/blogs?page=1");
         } else {
-            res.render("getBlogs", { blogs: foundBlogs, moment: moment });
+            res.render("getBlogs", { blogs: foundBlogs, moment: moment, next: parseInt(page) + 1 });
         }
     })
         .limit(limit)

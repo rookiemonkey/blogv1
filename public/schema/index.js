3ebb91f0075ca153097f7bdd06a1b14ca1c06291
rookiemonkey <kevinroirigorbasina@pm.me>
2020-08-08 16:27:45 +0800

    added some options for the mongo db connection

--- public/schema/index.js
+++ public/schema/index.js
@@ -6,9 +6,9 @@ const mongoose = require('mongoose');
 // =============================================
 // SETUP DATABASE CONNECTION
 // =============================================
-mongoose.connect(process.env.DBURL, {useNewUrlParser: true});
-mongoose.connection.on("error", () => {console.log("something went wrong upon connecting to database")});
-mongoose.connection.on("open", () => {console.log("established connection to database. TIMESTAMP: ", Date())});
+mongoose.connect(process.env.DBURL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on("error", () => { console.log("something went wrong upon connecting to database") });
+mongoose.connection.on("open", () => { console.log("established connection to database. TIMESTAMP: ", Date()) });
 
 // =============================================
 // EXPORTS
